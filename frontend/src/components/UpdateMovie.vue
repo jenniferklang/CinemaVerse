4ac156b2f894573b4c676c6dfa4330faa61f48f0
ponpon <pontus00valdes@gmail.com>
2023-05-09 15:58:48 +0200

    routes

--- frontend/src/components/UpdateMovie.vue
+++ frontend/src/components/UpdateMovie.vue
@@ -1,11 +1,19 @@
 <template>
   <form @submit.prevent="updateMovieRating">
     <select v-model="selectedMovieId">
-      <option v-for="movie in movies" :key="movie.movieId" :value="movie.movieId">{{ movie.movieName }}</option>
+      <option
+        v-for="movie in movies"
+        :key="movie.movieId"
+        :value="movie.movieId"
+      >
+        {{ movie.movieName }}
+      </option>
     </select>
 
     <select v-model="selectedMovieRating">
-      <option v-for="rating in ratings" :key="rating" :value="rating">{{ rating }}</option>
+      <option v-for="rating in ratings" :key="rating" :value="rating">
+        {{ rating }}
+      </option>
     </select>
 
     <button type="submit">Update rating</button>
@@ -14,99 +22,99 @@
 </template>
 
 <script>
-export default {
-  name: 'MovieDropdown',
-  data() {
-    return {
-      movies: [],
-      selectedMovieId: null,
-      selectedMovieRating: null,
-      ratings: [1, 2, 3, 4, 5],
-      selectedRating: null,
-    };
-  },
-  mounted() {
-    // Hämta filmer från API
-    fetch('http://localhost:3000/movie')
-      .then(response => response.json())
-      .then(data => {
-        this.movies = data;
-        this.selectedMovieId = data[0].movieId;
-        this.selectedMovieRating = data[0].ratingNumber;
-      })
-      .catch(error => {
-        console.error('Error fetching movies:', error);
-      });
-  },
-  watch: {
-    selectedMovieId() {
-      const selectedMovie = this.movies.find(movie => movie.movieId === this.selectedMovieId);
-      this.selectedMovieRating = selectedMovie.ratingNumber;
+  export default {
+    name: 'MovieDropdown',
+    data() {
+      return {
+        movies: [],
+        selectedMovieId: null,
+        selectedMovieRating: null,
+        ratings: [1, 2, 3, 4, 5],
+        selectedRating: null
+      }
     },
-  },
-  methods: {
-
-    updateMovieRating() {
-      const { selectedMovieId, selectedMovieRating } = this;
-      fetch('http://localhost:3000/admin', {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          movieId: selectedMovieId,
-          movieRatingId: selectedMovieRating,
-        }),
-      })
-        .then(response => response.json())
-        .then(data => {
-          console.log('Updated movie rating:', data);
+    mounted() {
+      // Hämta filmer från API
+      fetch('http://localhost:3000/movie')
+        .then((response) => response.json())
+        .then((data) => {
+          this.movies = data
+          this.selectedMovieId = data[0].movieId
+          this.selectedMovieRating = data[0].ratingNumber
         })
-        .catch(error => {
-          console.error('Error updating movie rating:', error);
-        });
-     },
-
-     deleteMovie() {
-      const { selectedMovieId, selectedMovieRating } = this;
-      fetch('http://localhost:3000/admin', {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          movieId: selectedMovieId,
-          movieRatingId: selectedMovieRating,
-        }),
-      })
-        .then(response => response.json())
-        .then(data => {
-          console.log('Updated movie rating:', data);
+        .catch((error) => {
+          console.error('Error fetching movies:', error)
         })
-        .catch(error => {
-          console.error('Error updating movie rating:', error);
-        });
-     },
+    },
+    watch: {
+      selectedMovieId() {
+        const selectedMovie = this.movies.find(
+          (movie) => movie.movieId === this.selectedMovieId
+        )
+        this.selectedMovieRating = selectedMovie.ratingNumber
+      }
+    },
+    methods: {
+      updateMovieRating() {
+        const { selectedMovieId, selectedMovieRating } = this
+        fetch('http://localhost:3000/admin', {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            movieId: selectedMovieId,
+            movieRatingId: selectedMovieRating
+          })
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            console.log('Updated movie rating:', data)
+          })
+          .catch((error) => {
+            console.error('Error updating movie rating:', error)
+          })
+      },
 
+      deleteMovie() {
+        const { selectedMovieId, selectedMovieRating } = this
+        fetch('http://localhost:3000/admin', {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            movieId: selectedMovieId,
+            movieRatingId: selectedMovieRating
+          })
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            console.log('Updated movie rating:', data)
+          })
+          .catch((error) => {
+            console.error('Error updating movie rating:', error)
+          })
+      }
 
-    //async deleteMovie() {
-    //   try {
+      //async deleteMovie() {
+      //   try {
 
-    //     const response = await fetch('http://localhost:3000/admin', {
-    //       method: 'DELETE',
+      //     const response = await fetch('http://localhost:3000/admin', {
+      //       method: 'DELETE',
 
-    //       body: JSON.stringify({
-    //       movieGenreMId: this.selectedMovieId,
-    //       movieId: this.selectedMovieId,
-    //     })
-    //     });
-    //     const data = await response.json();
-    //     console.log(data);
+      //       body: JSON.stringify({
+      //       movieGenreMId: this.selectedMovieId,
+      //       movieId: this.selectedMovieId,
+      //     })
+      //     });
+      //     const data = await response.json();
+      //     console.log(data);
 
-    //   } catch (error) {
-    //     console.error(error);
-    //   }
-    // },
-  },
-};
+      //   } catch (error) {
+      //     console.error(error);
+      //   }
+      // },
+    }
+  }
 </script>
