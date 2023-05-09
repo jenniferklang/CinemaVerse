5901854b469ddb39e94cef391bdbbcb3521a3480
Viktor Mattsson <viktor.a.mattsson@gmail.com>
2023-05-09 13:04:19 +0200

    add functionality for reset database, update rating and delete movie

--- frontend/src/views/AdminView.vue
+++ frontend/src/views/AdminView.vue
@@ -1,1 +1,18 @@
-<template><h1>Admin</h1></template>
+<script>
+  import ResetDb from '../components/ResetDb.vue'
+  import UpdateMovie from '../components/UpdateMovie.vue';
+
+
+  export default {
+    components: {
+    ResetDb,
+    UpdateMovie
+}
+  }
+</script>
+
+
+<template><h1>Admin</h1>
+<ResetDb />
+<UpdateMovie />
+</template>
